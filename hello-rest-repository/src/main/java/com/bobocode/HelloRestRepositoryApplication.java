366d81caae10a35bb2e804e423a07b200d9e6f13
tboychuk <tarass.boychuk@gmail.com>
2018-10-09 22:20:45 +0300

    Add new tutorial "Hello REST Repository"

---
+++ hello-rest-repository/src/main/java/com/bobocode/HelloRestRepositoryApplication.java
@@ -0,0 +1,12 @@
+package com.bobocode;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloRestRepositoryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HelloRestRepositoryApplication.class, args);
+    }
+}
---
+++ hello-rest-repository/src/main/java/com/bobocode/jpa/AccountRepository.java
@@ -0,0 +1,8 @@
+package com.bobocode.jpa;
+
+
+import com.bobocode.model.Account;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface AccountRepository extends JpaRepository<Account, Long> {
+}
