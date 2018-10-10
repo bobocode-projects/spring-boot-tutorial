4a653279757b679ad30566382bc07c055d5cda75
tboychuk <tarass.boychuk@gmail.com>
2018-10-10 15:26:05 +0300

    Remove some parts to be shown on demo

--- hello-rest-repository/src/main/java/com/bobocode/jpa/AccountRepository.java
+++ hello-rest-repository/src/main/java/com/bobocode/jpa/AccountRepository.java
@@ -2,17 +2,8 @@ package com.bobocode.jpa;
 
 
 import com.bobocode.model.Account;
-import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
-import org.springframework.data.rest.core.annotation.RestResource;
-
-import java.util.List;
 
 public interface AccountRepository extends JpaRepository<Account, Long> {
 
-    @RestResource(path = "/email")
-    @Query("select a from Account a where a.email like concat('%', '@', :domain)")
-    List<Account> findByEmailDomain(@Param("domain") String domain, Pageable pageable);
 }
